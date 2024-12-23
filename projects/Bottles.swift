63d408f6853a19b18b22bd51e18346d83ea72632
João Felipe <joaofsp.games@gmail.com>
2024-12-23 19:12:07 -0300

    Milk bottles in swift

---
+++ projects/Bottles.swift
@@ -0,0 +1,8 @@
+
+var numBottles : Int = 99
+
+while numBottles > 0 {
+  print("(\(numBottles)) bottles of milk on the wall, \(numBottles) bottles of milk! You take one down and pass it around, (\(numBottles)) bottles of milk on the wall!")
+  numBottles -= 1
+}
+print ("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!\nGo to the store and buy some more,\n99 bottles of milk on the wall!")
