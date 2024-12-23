fb8e6b1b41b8fd83017f4b78b41009bcdf8a11f1
João Felipe <joaofsp.games@gmail.com>
2024-12-23 18:51:42 -0300

    Aula 5: Loops(For in, while, break, continue)(100%)

--- Aulas/Aula5.swift
+++ Aulas/Aula5.swift
@@ -149,3 +149,18 @@ while total < 50 {
   total += diceRoll
 }
 */
+var guess = Int.random(in: 1...10)
+var magicNum = Int.random(in: 1...10)
+
+// Add your code here ✨
+while guess != magicNum {
+  guess = Int.random(in: 1...10)
+  magicNum = Int.random(in: 1...10)
+  print("You guessed \(guess), and the number was \(magicNum)")
+}
+
+
+
+
+print("You're right it was \(guess)!")
+
